05edcd38d48172225d17abcca67ca5ac6f2d4d24
Alexander P Nohe <anohe@esri.com>
2014-02-13 12:27:56 -0800

    Updated Add-In
    It doesn't work still :-|

--- portalpy-addin/Install/portalpy-addin_addin_3.py
+++ portalpy-addin/Install/portalpy-addin_addin_3.py
@@ -9,9 +9,32 @@ class listFolders2(object):
     def __init__(self):
         self.enabled = True
         self.shape = "NONE" # Can set to "Line", "Circle" or "Rectangle" for interactive shape drawing and to activate the onLine/Polygon/Circle event sinks.
-    def onClick(self):
-        x = portalpy.Portal("ess.maps.arcgis.com", "anohe_ess5","GeogWorks3!")
-        for i in x.folders("anohe_ess5"): print i['title']
+    def onMouseDown(self, x, y, button, shift):
+        pass
+    def onMouseDownMap(self, x, y, button, shift):
+        pass
+    def onMouseUp(self, x, y, button, shift):
+        pass
+    def onMouseUpMap(self, x, y, button, shift):
+        pass
+    def onMouseMove(self, x, y, button, shift):
+        pass
+    def onMouseMoveMap(self, x, y, button, shift):
+        pass
+    def onDblClick(self):
+        pass
+    def onKeyDown(self, keycode, shift):
+        pass
+    def onKeyUp(self, keycode, shift):
+        pass
+    def deactivate(self):
+        pass
+    def onCircle(self, circle_geometry):
+        pass
+    def onLine(self, line_geometry):
+        pass
+    def onRectangle(self, rectangle_geometry):
+        pass
 
 class listFoldersExt(object):
     """Implementation for portalpy-addin_addin.extension10 (Extension)"""
