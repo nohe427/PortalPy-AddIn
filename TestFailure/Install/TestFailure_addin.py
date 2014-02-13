64e4dc99f2edcb581fded93a5c71e634fedc7024
Alexander P Nohe <anohe@esri.com>
2014-02-13 15:25:17 -0800

    Added a Directory.

--- TestFailure/Install/TestFailure_addin.py
+++ TestFailure/Install/TestFailure_addin.py
@@ -15,4 +15,5 @@ class ButtonClass1(object):
         #value = pythonaddins.OpenDialog('Credentials', True, r'C:\'', 'Add')
         #I am thinking of just creating tools to do all of this then prompting
         #the button with: pythonaddins.GPToolDialog(toolbox, tool_name)
-        print x.folders("anohe_ess5")
+        for i in x.folders("anohe_ess5"):
+            print i['title']
---
+++ Tools/ReadMe.txt
@@ -0,0 +1,1 @@
+This is where the script tools to be incorporated in the toolbar will go.
