b06ba17ca4c73a67290ef0323632fc2f91c969c3
AshleyDesktop <astalzer@esri.com>
2014-02-15 21:07:53 -0800

    Modified script on my machine
    Modified script to run on my machine. Reverted back to custom tool Nohe
    made.

--- 1021version/TestFailure/Install/TestFailure_addin_2.py
+++ 1021version/TestFailure/Install/TestFailure_addin_2.py
@@ -1,7 +1,12 @@
 import arcpy
 import pythonaddins
 import sys
-sys.path.append(".\Tools\Tools")
+#sys.path.append(".\Tools\Tools")
+
+#To get it working on Ash's home machine.
+tbxpath = r".\TestFailure\Install\Tools\Tools"
+sys.path.append(tbxpath)
+
 import portalpy
 
 global portalLogin
@@ -12,7 +17,7 @@ class ButtonClass1(object):
         self.enabled = True
         self.checked = False
     def onClick(self):
-        portalLogin = GPToolDialog(r".\Tools\Toolbox.tbx", "SignOn")
+        portalLogin = GPToolDialog(tbxpath + "\\Toolbox.tbx", "SignOn")
         #This is used to select datasets which is a possibility
         #value = pythonaddins.OpenDialog('Credentials', True, r'C:\'', 'Add')
         #I am thinking of just creating tools to do all of this then prompting
--- 1021version/TestFailure/Install/TestFailure_addin_4.py
+++ 1021version/TestFailure/Install/TestFailure_addin_4.py
@@ -1,29 +1,23 @@
-import arcpy, pythonaddins, sys
-
-#sys.path.append(".\Tools\Tools")
-#2/15 Ash: Above searches for Tools\Tools in C:\Windows\System32 on Win8.
-
-#Used to get SignOn working on Ash's machine:
-tbxpath = r"C:\Users\Ashley\Documents\GitHub\PortalPy-AddIn\1021version\TestFailure\Install\Tools"
-sys.path.append(tbxpath + "\\Tools")
-
+import arcpy
+import pythonaddins
+import sys
+sys.path.append(".\Tools\Tools")
 import portalpy
 
 global portalLogin
 
-class SignIn(object):
+tbxpath = r"C:\Program Files (x86)\ArcGIS\Desktop10.2\ArcToolbox\Toolboxes"
+
+class ButtonClass1(object):
     """Implementation for TestFailure_addin.button (Button)"""
     def __init__(self):
-        
         self.enabled = True
         self.checked = False
-
     def onClick(self):
 
-        #2/15 Ash: Reverted back to script tool Alex created in Install\Tools\Toolbox.
-        #          Added pythonaddins to GPToolDialog and changed input parameter.
-        
-        portalLogin = pythonaddins.GPToolDialog(tbxpath + "\\Toolbox.tbx", "SignOn")
+        #Ash: I messed up this button because I wanted to see a sign on/ put something on git.
+
+        portalLogin = pythonaddins.GPToolDialog(tbxpath + "\\Server Tools.tbx", "SignInToPortal")
         
         #This is used to select datasets which is a possibility
         #value = pythonaddins.OpenDialog('Credentials', True, r'C:\'', 'Add')
@@ -36,3 +30,5 @@ class SignIn(object):
         else:
             print "False"
 
+"""
+"""
