cc57e3d49bc7df9494ece71bdfc437210d57e9db
AshleyDesktop <astalzer@esri.com>
2014-02-17 22:18:05 -0800

    Added code to 102 version to create output.txt
    where output.txt is a list of services in the org (ESS only).

--- 102version/test/Install/test_addin_5.py
+++ 102version/test/Install/test_addin_5.py
@@ -4,21 +4,6 @@ import sys
 sys.path.append(r"C:\Python27\Lib\site-packages")
 from portalpy import *
 
-#Ash 2/17: Import modules to read a webpage
-import shutil, os, time, datetime, math, urllib
-from array import array
-
-#Ash 2/17: Rest endpoint url (for any organization)
-list_services_path1 = r"http://services.arcgis.com"
-list_services_path2 = r"ArcGIS/rest/services?f=pjson"  #Converted to JSON format
-
-#Ash 2/17: ESS specific org ID. How do we find out this info for all orgs?
-ESS_org_ID = "//Wl7Y1m92PbjtJs5n//"
-
-#Ash 2/17: Output location for Nohe
-output = r"C:\Users\AlexanderN\Documents\GitHub\PortalPy-AddIn\102version\test\Install\Output\output.txt"
-
-
 global portalLogin
 global portalID
 #Get bad syntac error when declaring global portalLogin = () or portalLogin = portalpy.Portal()
@@ -36,21 +21,6 @@ class SignOn(object):
             other classes that we create from this.
         """
         pythonaddins.GPToolDialog(r"C:\Users\AlexanderN\Documents\GitHub\PortalPy-AddIn\102version\test\Install\Tools\Toolbox.tbx", "SignOn")
-
-        #Ash 2/17: Creates an output.txt file that contains the names of all services in the organization.
-
-        FILE = open(output, "w")
-
-        filehandle = urllib.urlopen(list_services_path1 + ESS_org_ID + list_services_path2)
-        for lines in filehandle.readlines():
-            #[16:-5]      "name" : "      
-            if '"name"' in lines:
-                FILE.write(lines[16:-5])
-                FILE.write('\n')
-
-        FILE.close()
-        
-        print "Check your output folder for a list of services in your organization."
 
 class folderList(object):
     """Implementation for test_addin.button (Button)"""
