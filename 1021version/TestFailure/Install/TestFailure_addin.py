3787668625e83cd2fcf75589fce5da3fcaffacbb
AshleyDesktop <astalzer@esri.com>
2014-02-18 22:56:36 -0800

    Obtained org ID used to read rest endpoint url
    - <portalpy object>.info().properties['id']
    - Updated 102 version

--- 1021version/TestFailure/Install/TestFailure_addin.py
+++ 1021version/TestFailure/Install/TestFailure_addin.py
@@ -1,13 +1,14 @@
-import arcpy, pythonaddins, sys, shutil, os, time, datetime, math, urllib
+import arcpy, pythonaddins, sys, os, urllib
 from array import array
 
 tbxpath = r"C:\Users\Ashley\Documents\GitHub\PortalPy-AddIn\1021version\TestFailure\Install\Tools"
 sys.path.append(tbxpath + "\\Tools")
 
-list_services_path1 = r"http://services.arcgis.com"
-list_services_path2 = r"ArcGIS/rest/services?f=pjson"  #Converted to JSON format
+list_services_path1 = r"http://services.arcgis.com/"
+list_services_path2 = r"/ArcGIS/rest/services?f=pjson"
 
-ESS_org_ID = "//Wl7Y1m92PbjtJs5n//"    #How do we find out this info?
+#2/18 Ash: Accessing rest URL will only print public services. I suppose that is all the rage with tokens.
+#          Need to get a list of item ids.
 
 output = r"C:\Users\Ashley\Documents\GitHub\PortalPy-AddIn\1021version\TestFailure\Install\Output\output.txt"
 
@@ -23,12 +24,19 @@ class SignIn(object):
         self.checked = False
 
     def onClick(self):
-        
-        portalLogin = pythonaddins.GPToolDialog(tbxpath + "\\Toolbox.tbx", "SignIn")
+
+        #pythonaddins.GPToolDialog(tbxpath + "\\Toolbox.tbx", "SignIn")
+
+        URL = r"http://www.arcgis.com/"
+        user = 'eek'
+        password = 'eek'
+
+        portalLogin = portalpy.Portal(URL, user, password)
+        org_ID = portalLogin.info().properties['id']
 
         FILE = open(output, "w")
-
-        filehandle = urllib.urlopen(list_services_path1 + ESS_org_ID + list_services_path2)
+        
+        filehandle = urllib.urlopen(list_services_path1 + org_ID + list_services_path2)
         for lines in filehandle.readlines():
             #[16:-5]      "name" : "      
             if '"name"' in lines:
@@ -36,7 +44,4 @@ class SignIn(object):
                 FILE.write('\n')
 
         FILE.close()
-        
-        print "Check your output folder for a list of services in your organization."
-
-
+        print 'Woot.'
